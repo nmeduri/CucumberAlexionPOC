847f3b0e0ed60fca0b14607069dd3b2ffa441adb
nmeduri <43102805+nmeduri@users.noreply.github.com>
2020-06-19 09:42:29 +0530

    Update NewsandMediaLinkTestSteps.java

--- com.alexion.NewsandMedia/src/main/java/com/alexion/stepdefenitions/NewsandMediaLinkTestSteps.java
+++ com.alexion.NewsandMedia/src/main/java/com/alexion/stepdefenitions/NewsandMediaLinkTestSteps.java
@@ -17,35 +17,35 @@ public class NewsandMediaLinkTestSteps extends BrowserSetup {
 	}
 	
 	@Given("^User is on Alexion Home Page$")
-	public void user_is_on_Alexion_Home_Page() {
+	public void userisonAlexionHomepage() {
 		BrowserSetup.launchBrowser();
 	}
 
 	@When("^User  click on NEWS&MEDIA Link$")
-	public void user_click_on_NEWS_MEDIA_Link() {
+	public void userclickonNewsandMedialink() {
 		BrowserSetup.clickLink();
 	}
 
 	@Then("^NEWS&MEDIA page should be displayed$")
-	public void news_MEDIA_page_should_be_displayed() {
+	public void newsandmediapageDisplayed() {
 		BrowserSetup.assertText();
 	}
 
 	@And("^User selects option from NEWS CATEGORY dropdown and Year from YEAR dropdown$")
-	public void user_selects_option_from_NEWS_CATEGORY_dropdown_and_Year_from_YEAR_dropdown()
+	public void userselectsCategory()
 			throws InterruptedException {
 		BrowserSetup.selectOptionsFromDropdown();
 	}
 
 	@When("^User click on Filter button$")
-	public void user_click_on_Filter_button() {
+	public void userclickButton() {
 		BrowserSetup.clickButton();
 	}
 
 	@Then("^User selected Year News should be displayed on the same page$")
-	public void user_selected_Year_News_should_be_displayed_on_the_same_page() {
+	public void userselectsYear() {
 		BrowserSetup.closeBrowser();
-		System.out.println("NewsandMedia Feature Successfully Executed....");
+		
 	}
 	@After
 	public void tearDown() {
